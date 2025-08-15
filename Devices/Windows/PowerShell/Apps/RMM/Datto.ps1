91d6b68c9c9aef83ca90d513aa55bc5bfd5d7efb
simislearning <64547883+simislearning@users.noreply.github.com>
2025-08-14 20:30:43 -0700

    Update Datto.ps1

--- Devices/Windows/PowerShell/Apps/RMM/Datto.ps1
+++ Devices/Windows/PowerShell/Apps/RMM/Datto.ps1
@@ -1,4 +1,4 @@
 # --- Action: Install Microsoft Office ---
 # Placeholder for Microsoft Office installation script
-Write-Host 'Installing Microsoft Office...'
+Write-Host 'Installing Datto...'
 # Add actual script logic here
