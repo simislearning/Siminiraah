2e7fae6c90e8df38f4a3506f5a26421fa501b44d
simislearning <64547883+simislearning@users.noreply.github.com>
2025-08-14 23:23:48 -0700

    Update CreateLocalAccount.ps1

--- Devices/Windows/PowerShell/Apps/Config/CreateLocalAccount.ps1
+++ Devices/Windows/PowerShell/Apps/Config/CreateLocalAccount.ps1
@@ -2,7 +2,6 @@ $Siminiraah_Username = ''
 $Siminiraah_Password = ''
 
 
-# --- Action: Create Local Account ---
 Write-Host "Creating local account for $Siminiraah_Username..."
 
 Write-Host "Account for $Siminiraah_Username created successfully."
