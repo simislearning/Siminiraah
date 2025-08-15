0aa5b09f9d6dd088482d52a7aeb23a94f01f2f93
simislearning <64547883+simislearning@users.noreply.github.com>
2025-08-14 20:26:37 -0700

    Create SentinelOne.sh

---
+++ Devices/Mac/Bash/Apps/Security/SentinelOne.sh
@@ -0,0 +1,9 @@
+# --- Workflow Variables ---
+export SiteToken=''
+# -------------------------
+
+# --- Action: Install SentinelOne ---
+#!/bin/bash
+# Installs SentinelOne Agent
+echo "Installing SentinelOne with Site Token: $SiteToken"
+# Add actual script logic here
