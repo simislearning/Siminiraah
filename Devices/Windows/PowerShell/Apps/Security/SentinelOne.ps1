b7dad6e00a29ef893c715519ddfd223e2f6043b6
simislearning <64547883+simislearning@users.noreply.github.com>
2025-08-14 20:31:33 -0700

    Create SentinelOne.ps1

---
+++ Devices/Windows/PowerShell/Apps/Security/SentinelOne.ps1
@@ -0,0 +1,8 @@
+# --- Workflow Variables ---
+$SiteToken = ''
+# -------------------------
+
+# --- Action: Install SentinelOne ---
+# Installs SentinelOne Agent
+Write-Host "Installing SentinelOne with Site Token: $SiteToken"
+# Add actual script logic here
